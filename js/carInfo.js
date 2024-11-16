c5e258585c04b6ce6759961939b7d9505a323c9a
gwiri <gwiri10@naver.com>
2024-11-16 22:28:09 +0900

    uu

--- js/carInfo.js
+++ js/carInfo.js
@@ -54,10 +54,8 @@ async function fetchCarInfo(data) {
     $("#air-conditioning").html(ariconNm);
     $("#door-count").html(data.door + '개');
 
-    $("#totalPrice").val(data.price);
-
     //인수위치 세팅
-    let companyNm = await fetchCompanysDocument(data.companyCd*1);
+    let companyNm = await fetchCompanysDocument(data.companyCd * 1);
     $("#rentSpot").html(companyNm);
 }
 
@@ -85,7 +83,7 @@ $("#pickupDate").daterangepicker({
 
 $("#pickupDate").on('change', function (ev, picker) {
     let dateArray = $("#pickupDate").val().split(' ~ ');
-    if(dateArray.length > 1 ){
+    if (dateArray.length > 1) {
         $("#pickupDate").val(dateArray[0]);
         $("#returnDate").val(dateArray[1]);
     }
@@ -93,160 +91,186 @@ $("#pickupDate").on('change', function (ev, picker) {
     getDateDiff();
 });
 
-const getDateDiff = async() => {
+const getDateDiff = async () => {
     //대여일시와 반납일시간의 시간 구하기
     let date1 = new Date($("#pickupDate").val());
     let date2 = new Date($("#returnDate").val());
 
     //예약불가 기간 안에 선택한 값이 들어가는지 확인
-    let isDateInRangeResultCancle = isDateInRange(date1, date2, 2) ;
+    let isDateInRangeResultCancle = isDateInRange(date1, date2, 2);
     $("#downDataCheck").val(isDateInRangeResultCancle);
-    if(isDateInRangeResultCancle){
+    if (isDateInRangeResultCancle) {
         let msg = "2024-11-20 부터 2024-11-23 은 예약불가 기간입니다."
-        if($("#downData").val()!=undefined && $("#downData").val()!=""){
-            msg = $("#downData").val()+" 은 예약불가 기간입니다."
+        if ($("#downData").val() != undefined && $("#downData").val() != "") {
+            msg = $("#downData").val() + " 은 예약불가 기간입니다."
         }
         alert(msg);
         return;
     }
 
-    let diffDate = Math.abs((date1.getTime() - date2.getTime()) / (1000 * 60 * 60 )); // 밀리세컨 * 초 * 분 = 시간
+    let diffDate = Math.abs((date1.getTime() - date2.getTime()) / (1000 * 60 * 60)); // 밀리세컨 * 초 * 분 = 시간
 
     //대여일시와 반납일시간의 날짜 구하기(나머지가 있다면 +1)
-    let diffDay = parseInt( diffDate / 24 );
+    let diffDay = parseInt(diffDate / 24);
     let totalPrice = 0;
     let extraDate = diffDate % 24;
     //총 대여시간을 24로 나눈 나머지 시간이 5시간을 초과하면 1박추가로 계산
-    if(extraDate != 0){
-        if(extraDate > 5){
-            diffDay += 1; 
+    if (extraDate != 0) {
+        if (extraDate > 5) {
+            diffDay += 1;
             extraDate = 0;
         }
-    } 
-
-    totalPrice = ($("#totalPrice").val() * diffDay) + extraDate*11000;
-
-    //사전결제비율
-    let calRate = $("#rate").val()==""? 0.2 : $("#rate").val()*0.01;
-    let beforePrice = totalPrice * calRate;
-    let afterPrice = (totalPrice* 1 - beforePrice) / 10;
+    }
 
     let data = await fetchOneDocument(id);
     let addPrice = 0;
-    //기간별 추가 요금 세팅
+    //기간별 요금 세팅
     //1. 비수기
-    let rage1 = data.upData1 != undefined ? data.upData1 : '25/01/06 - 25/01/24'
-    let isDateInRangeResult = isAddPriceDateInRange(date1, date2, rage1) ;
-    if(isDateInRangeResult){
-        if(diffDay == 1){
+    let rage1 = data.upData1 != undefined ? data.upData1 : '24/01/06 - 25/01/24'
+    let isDateInRangeResult = isAddPriceDateInRange(date1, date2, rage1);
+    if (isDateInRangeResult) {
+        if (diffDay == 1) {
             addPrice = data.addprice24_1 != undefined ? data.addprice24_1 : 5500
-        }else if(diffDay == 2){
+        } else if (diffDay == 2) {
             addPrice = data.addprice48_1 != undefined ? data.addprice48_1 : 10800
         }
-        else if(diffDay == 3){
+        else if (diffDay == 3) {
             addPrice = data.addprice72_1 != undefined ? data.addprice72_1 : 16000
         }
-        else if(diffDay == 4){
+        else if (diffDay == 4) {
             addPrice = data.addprice96_1 != undefined ? data.addprice96_1 : 21100
         }
-        else{
+        else {
             addPrice = data.addprice100_1 != undefined ? data.addprice100_1 : 5000
-            addPrice = addPrice*diffDay
+            addPrice = addPrice * diffDay
         }
     }
 
     //2. 준성수기
     let rage2 = data.upData2 != undefined ? data.upData2 : '25/07/01 - 25/07/02'
-    let isDateInRangeResult2 = isAddPriceDateInRange(date1, date2, rage2) ;
-    if(isDateInRangeResult2){
-        if(diffDay == 1){
+    let isDateInRangeResult2 = isAddPriceDateInRange(date1, date2, rage2);
+    if (isDateInRangeResult2) {
+        if (diffDay == 1) {
             addPrice = data.addprice24_2 != undefined ? data.addprice24_2 : 6000
-        }else if(diffDay == 2){
+        } else if (diffDay == 2) {
             addPrice = data.addprice48_2 != undefined ? data.addprice48_2 : 11900
         }
-        else if(diffDay == 3){
+        else if (diffDay == 3) {
             addPrice = data.addprice72_2 != undefined ? data.addprice72_2 : 16000
         }
-        else if(diffDay == 4){
+        else if (diffDay == 4) {
             addPrice = data.addprice96_2 != undefined ? data.addprice96_2 : 21100
         }
-        else{
+        else {
             addPrice = data.addprice100_2 != undefined ? data.addprice100_2 : 5600
-            addPrice = addPrice*diffDay
+            addPrice = addPrice * diffDay
         }
     }
 
     //2. 성수기
     let rage3 = data.upData3 != undefined ? data.upData3 : '25/08/01 - 25/08/02'
-    let isDateInRangeResult3 = isAddPriceDateInRange(date1, date2, rage3) ;
-    if(isDateInRangeResult3){
-        if(diffDay == 1){
+    let isDateInRangeResult3 = isAddPriceDateInRange(date1, date2, rage3);
+    if (isDateInRangeResult3) {
+        if (diffDay == 1) {
             addPrice = data.addprice24_3 != undefined ? data.addprice24_3 : 7500
-        }else if(diffDay == 2){
+        } else if (diffDay == 2) {
             addPrice = data.addprice48_3 != undefined ? data.addprice48_3 : 14900
         }
-        else if(diffDay == 3){
+        else if (diffDay == 3) {
             addPrice = data.addprice72_3 != undefined ? data.addprice72_3 : 16000
         }
-        else if(diffDay == 4){
+        else if (diffDay == 4) {
             addPrice = data.addprice96_3 != undefined ? data.addprice96_3 : 21100
         }
-        else{
+        else {
             addPrice = data.addprice100_3 != undefined ? data.addprice100_3 : 7100
-            addPrice = addPrice*diffDay
+            addPrice = addPrice * diffDay
         }
     }
 
     //4. 극성수기
     let rage4 = data.upData4 != undefined ? data.upData4 : '25/09/01 - 25/09/02'
-    let isDateInRangeResult4 = isAddPriceDateInRange(date1, date2, rage4) ;
-    if(isDateInRangeResult4){
-        if(diffDay == 1){
+    let isDateInRangeResult4 = isAddPriceDateInRange(date1, date2, rage4);
+    if (isDateInRangeResult4) {
+        if (diffDay == 1) {
             addPrice = data.addprice24_4 != undefined ? data.addprice24_4 : 10000
-        }else if(diffDay == 2){
+        } else if (diffDay == 2) {
             addPrice = data.addprice48_4 != undefined ? data.addprice48_4 : 19900
         }
-        else if(diffDay == 3){
+        else if (diffDay == 3) {
             addPrice = data.addprice72_4 != undefined ? data.addprice72_4 : 16000
         }
-        else if(diffDay == 4){
+        else if (diffDay == 4) {
             addPrice = data.addprice96_4 != undefined ? data.addprice96_4 : 21100
         }
-        else{
+        else {
             addPrice = data.addprice100_4 != undefined ? data.addprice100_4 : 9600
-            addPrice = addPrice*diffDay
+            addPrice = addPrice * diffDay
         }
     }
-    
-    afterPrice += addPrice;
+
+    //5. 속해있는 곳이 없으면 비수기 요금으로 하겠음
+    let flag = isDateInRangeResult || isDateInRangeResult2 || isDateInRangeResult3 || isDateInRangeResult4
+    if (!flag) {
+        if (diffDay == 1) {
+            addPrice = data.addprice24_1 != undefined ? data.addprice24_1 : 5500
+        } else if (diffDay == 2) {
+            addPrice = data.addprice48_1 != undefined ? data.addprice48_1 : 10800
+        }
+        else if (diffDay == 3) {
+            addPrice = data.addprice72_1 != undefined ? data.addprice72_1 : 16000
+        }
+        else if (diffDay == 4) {
+            addPrice = data.addprice96_1 != undefined ? data.addprice96_1 : 21100
+        }
+        else {
+            addPrice = data.addprice100_1 != undefined ? data.addprice100_1 : 5000
+            addPrice = addPrice * diffDay
+        }
+    }
+
+    totalPrice += addPrice;
+
+    //환율 정보 가져오기 
+    let querySnapshot = await fetchData("rate");
+    let exchangeRate = 0;
+    querySnapshot.forEach((doc) => {
+        exchangeRate = doc.data().rate;
+    });
+
+    //사전결제비율
+    let calRate = $("#rate").val() == "" ? 0.2 : $("#rate").val() * 0.01;
+    let beforePrice = Math.ceil(totalPrice * calRate * exchangeRate / 10) * 10;
+    let afterPrice = totalPrice * (1-calRate)
+
     //예약하기 시 넘겨줄 데이터
     document.getElementById("afterPrice").value = afterPrice;
     document.getElementById("beforePrice").value = beforePrice;
     document.getElementById("diffDay").value = diffDay;
 
-    $("#rentDate").html("총 "+diffDate+"시간")
+    $("#rentDate").html("총 " + diffDate + "시간")
     $("#price").html("사전결제 :  " + beforePrice + '(원) + 현장결제 : ' + afterPrice + '(엔)');
 
 }
 
 function isDateInRange(date1, date2, flag) {
     let range = "";
-    if(flag == 1){
+    if (flag == 1) {
         range = $("#upData").val();
-    }else{
+    } else {
         range = $("#downData").val();
     }
-    
+
     let range1Start, range1End = "";
-    if(range == undefined || range == ""){
-        if(flag == 1){
+    if (range == undefined || range == "") {
+        if (flag == 1) {
             range1Start = '2025-01-06';
             range1End = '2025-01-24';
-        }else{
+        } else {
             range1Start = '2024-11-20';
             range1End = '2024-11-23';
         }
-    }else{
+    } else {
         let rangeArr = range.trim().split("-");
         range1Start = rangeArr[0];
         range1End = rangeArr[1];
@@ -256,14 +280,14 @@ function isDateInRange(date1, date2, flag) {
     const startDate = new Date(date1);
     const endDate = new Date(date2);
     const r1Start = new Date(range1Start);
-        r1Start.setHours(0); r1Start.setMinutes(0);
+    r1Start.setHours(0); r1Start.setMinutes(0);
     const r1End = new Date(range1End);
     r1End.setHours(0); r1End.setMinutes(0);
     // Check overlap with range 1
     const isInRange1 = startDate <= r1End && endDate >= r1Start;
 
     // Return true if either range matches
-    return isInRange1 
+    return isInRange1
 }
 
 
@@ -277,12 +301,12 @@ function isAddPriceDateInRange(date1, date2, range) {
     const startDate = new Date(date1);
     const endDate = new Date(date2);
     const r1Start = new Date(range1Start);
-        r1Start.setHours(0); r1Start.setMinutes(0);
+    r1Start.setHours(0); r1Start.setMinutes(0);
     const r1End = new Date(range1End);
-        r1End.setHours(0); r1End.setMinutes(0);
+    r1End.setHours(0); r1End.setMinutes(0);
     // Check overlap with range 1
     const isInRange1 = startDate <= r1End && endDate >= r1Start;
 
     // Return true if either range matches
-    return isInRange1 
+    return isInRange1
 }
